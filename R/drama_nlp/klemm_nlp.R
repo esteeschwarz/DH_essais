10fc44c8c520d4b374550243e01383d0a2662beb
estee <91368979+esteeschwarz@users.noreply.github.com>
2022-06-29 06:15:26 +0200

    not access directory, fail, path wirrwarr...

--- R/drama_nlp/klemm_nlp.R
+++ R/drama_nlp/klemm_nlp.R
@@ -13,6 +13,12 @@ library(DramaAnalysis)
 library(magrittr)
 library(igraph)
 ?installData
+installData(dataDirectory = "data/klemm_besuch/nlp",baseUrl = "https://github.com/esteeschwarz",remoteUrl = "https://github.com/esteeschwarz/DH_drama.git")
+installData(dataDirectory = "drama_local",baseUrl = "https://github.com/esteeschwarz",remoteUrl = "https://github.com/esteeschwarz/DH_drama.git")
+#wks.
 ?loadDrama
+loadDrama("data/klemm_besuch/nlp/OUT/csv/esuch_(Klemm)")
 ?installCollectionData
-installCollectionData(getOption("klemm_nlp"),"main","DH_essais","https://github.com/esteeschwarz/")
+installCollectionData(getOption("data/klemm_besuch/nlp"),"main","DH_drama","https://github.com/esteeschwarz/")
+### wo sucht der denn? > loadDrama("data/klemm_besuch/nlp/OUT/csv/esuch_(Klemm)")
+#/Users/guhl/QuaDramA/Data2/qd/csv/data/klemm_besuch/nlp/OUT/csv/esuch_(Klemm).UtterancesWithTokens.csv could not be loaded and was skipped
