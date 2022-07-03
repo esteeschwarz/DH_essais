2ecdc612d0db769c97f032a1b97dadd8d6760d49
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-07-03 12:48:07 +0200

    shiny hux21

--- R/shiny002/app.R
+++ R/shiny002/app.R
@@ -1,3 +1,14 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
 ui <- bootstrapPage(
   h3("URL components"),
   verbatimTextOutput("urlText"),
@@ -15,13 +26,14 @@ server <- function(input, output, session) {
           "hostname: ", session$clientData$url_hostname, "\n",
           "pathname: ", session$clientData$url_pathname, "\n",
           "port: ",     session$clientData$url_port,     "\n",
-          "search: ",   session$clientData$url_search,   "\n"
+          "search: ",   session$clientData$url_search,   "\n",
+          "mydummy", "HELLO FUCT!"
     )
   })
   
   # Parse the GET query string
   output$queryText <- renderText({
-    query <- parseQueryString(session$clientData$url_search)
+    query <- parseQueryString("whatdefuck")
     
     # Return a string with key-value pairs
     paste(names(query), query, sep = "=", collapse=", ")
