a9f66c59dfa26216ca9ac25b0304d557b266c9ad
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-09-25 13:53:40 +0200

    mv

--- sections/DD/klemm_HA/scripts/DTA_query_eval.R
+++ sections/DD/klemm_HA/scripts/DTA_query_eval.R
@@ -49,41 +49,6 @@ colnames(d9)<-nst
 dr<-rbind(d1,d2,d3,d4,d5,d6,d7,d8,d9)
 }
 df<-as.data.frame(getdta())
-dc<-getdta()
-#mannsperson, heurath, heyrath, heyraht, heirath, heiraht, heirat, hochzeit
-dc<-cbind(d1[2],d1[,1],d2[,1],d3[,1],d4[,1],d5[,1],d6[,1],d7[,1],d8[,1],d9[,1])
-dr<-rbind(d1[,1],d2[,1],d3[,1],d4[,1],d5[,1],d6[,1],d7[,1],d8[,1],d9[,1])
-d1[,1]
-dr<-c(d1[,1],d2[,1],d3[,1],d4[,1],d5[,1],d6[,1],d7[,1],d8[,1],d9[,1])
-
-#dc<-getdta()
-# #nst<-c("sum","year","token")
-# colnames(d1)<-nst
-# colnames(d2)<-nst
-# colnames(d3)<-nst
-# colnames(d4)<-nst
-# colnames(d5)<-nst
-# colnames(d6)<-nst
-# colnames(d7)<-nst
-# colnames(d8)<-nst
-# colnames(d1)<-nst
-# colnames(d1)<-nst
-# colnames(d1)
-# d1[,3]<-colnames(d1)[1]
-# d1
-# dr<-rbind(d1,d2,d3,d4,d5,d6,d7,d8,d9)
-# d1[,1]
-# colnames(dc)<-ns
-# rownames(dc)<-d1[,2]
-#ggplot(as.data.frame(dc))
-df<-as.data.frame(dc)
-#p <- ggplot(data=df,mapping=aes(x=year,
-                                y=count))
-#p
-#p + geom_point(group="token",color="black")
-#library(ggplot2)
 
 ggplot(df, aes(year, count, colour = token)) + 
   geom_line()
-#plot(df$year,df$count,type="l" )
-#help("plot")
