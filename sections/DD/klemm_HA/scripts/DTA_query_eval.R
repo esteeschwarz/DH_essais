a4b92804aa9a457edfd80832f0e6e8509b9422a4
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-09-25 14:22:07 +0200

    src script & plot DTA distribution

--- sections/DD/klemm_HA/scripts/DTA_query_eval.R
+++ sections/DD/klemm_HA/scripts/DTA_query_eval.R
@@ -5,33 +5,33 @@ ns<-c("year","mannsbild","mannsperson","heurath","heyrath","heyraht","heirath","
 #d<-"data/dhist-plot.perl-4.txt"
 getdta<-function(){
 
-d<-"data/dhist-plot.perl-4.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-4.txt"
 d1<-read.delim(d)
 #d1[,3]<-colnames(d1)[1]
 d1[,3]<-ns[2]
 
-d<-"data/dhist-plot.perl-5.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-5.txt"
 d2<-read.delim(d)
 d2[,3]<-ns[3]
-d<-"data/dhist-plot.perl-6.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-6.txt"
 d3<-read.delim(d)
 d3[,3]<-ns[4]
-d<-"data/dhist-plot.perl-7.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-7.txt"
 d4<-read.delim(d)
 d4[,3]<-ns[5]
-d<-"data/dhist-plot.perl-8.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-8.txt"
 d5<-read.delim(d)
 d5[,3]<-ns[6]
-d<-"data/dhist-plot.perl-9.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-9.txt"
 d6<-read.delim(d)
 d6[,3]<-ns[7]
-d<-"data/dhist-plot.perl-10.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-10.txt"
 d7<-read.delim(d)
 d7[,3]<-ns[8]
-d<-"data/dhist-plot.perl-11.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-11.txt"
 d8<-read.delim(d)
 d8[,3]<-ns[9]
-d<-"data/dhist-plot.perl-12.txt"
+d<-"https://raw.githubusercontent.com/esteeschwarz/DH_essais/main/sections/DD/klemm_HA/data/dhist-plot.perl-12.txt"
 d9<-read.delim(d)
 d9[,3]<-ns[10]
 
