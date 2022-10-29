d94c7ccb1a92575a1d46227a66d449036c96d1ad
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-10-29 17:45:25 +0200

    abgabe 17 transcripts

--- sections/HU-LX/20221015_transcript_chat_preprocessing(R-script)_v2.1.R
+++ sections/HU-LX/20221015_transcript_chat_preprocessing(R-script)_v2.1.R
@@ -17,9 +17,9 @@ library(stringi)
 library(clipr)
 # 1. global variables
 #mini
-#setwd("~/boxHKW/21S/DH/")
+setwd("~/boxHKW/21S/DH/")
 #lapsi, ewa
-setwd("~/boxHKW/UNI/21S/DH/")
+#setwd("~/boxHKW/UNI/21S/DH/")
 dirtext<-paste0(getwd(),"/local/HU-LX/000_SES_REFORMATTED_transcripts/Formatted with header info/text")
 codesource<-"/r-temp/codes_cpt3mod.csv"
 list.files(dirtext)
@@ -672,3 +672,8 @@ set<-codes_cpt
 # }
 # kids4
 # filelist2<-kids4
+# complete filelist of all converted files:
+# dirchat
+# filelist<-list.files(paste0(dirtext,dirchat),pattern="(\\.txt)")
+# write_clip(filelist)
+# writeLines(filelist,paste0(dirtemp,"/CHAT_transcripts_list.txt"))
