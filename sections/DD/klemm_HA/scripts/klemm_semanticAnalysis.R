21462440d65a3547d0cb3b44d080346b3897b4f6
esteeschwarz <gith@stschwarz.dialup.fu-berlin.de>
2022-11-05 13:04:19 +0100

    stuck

--- sections/DD/klemm_HA/scripts/klemm_semanticAnalysis.R
+++ sections/DD/klemm_HA/scripts/klemm_semanticAnalysis.R
@@ -7,6 +7,7 @@ library(jsonlite)
 library(purrr)
 library(stylo)
 library(igraph)
+library(network)
 
 
 src<-"https://dracor.org/api/corpora/ger/play/klemm-der-besuch/spoken-text-by-character.json"
@@ -43,9 +44,6 @@ col <- "orange" # für die Knotenfarbe
 V(net)$name<-dtatxt$label
 
 # Knotengröße ändern
-plot(net, edge.arrow.size=.4,vertex.label=NA, 
-     vertex.size=n)
-
 # Gekrümmte Kanten (edge.curved=.4) :
 plot(net, edge.arrow.size=.4,
      vertex.size=n, vertex.color=col,
@@ -61,4 +59,45 @@ library(networkD3)
 #stylo(corpus.dir = path2, gui = T)
 nodes<-x$list.of.nodes
 edges<-x$list.of.edges
-net2 <- graph_from_data_frame(d=edges, vertices=nodes,directed=FALSE) 
+net2 <- graph_from_data_frame(d=edges) 
+edges<- edges[,c("Source", "Target", "Weight")]
+ edges # überprüfen
+# # neue Kantenliste speichern
+# write.csv(edges_df, "ger000577-klemm-der-besuch_edges.csv", quote=FALSE, row.names=FALSE) # alte Kantenliste überschreiben
+# 
+# # Kantenliste neu einlesen: 
+# edges <- read.csv("ger000577-klemm-der-besuch_edges.csv", header=T, as.is=T) 
+#
+ 
+network(x,vertices=x$list.of.nodes)
+is.network(edges_df$Weight)
+
+# simple networks with vertices =======================================================
+simple_edge_df <- data.frame(
+  from = c("b", "c", "c", "d", "a"),
+  to = c("a", "b", "a", "a", "b"),
+  weight = c(1, 1, 2, 2, 3),
+  stringsAsFactors = FALSE
+)
+simple_edge_df
+simple_vertex_df <- data.frame(
+  name = letters[1:5],
+  residence = c("urban", "rural", "suburban", "suburban", "rural")
+)
+simple_vertex_df
+
+net3<-(as.network(simple_edge_df, vertices = simple_vertex_df))
+plot(net3, edge.arrow.size=.4,label=simple_vertex_df$name, 
+     vertex.size=n)
+simple_vertex_df$residence
+  plot(net4,label=nodes$id)
+
+  
+  a1<-data.frame(
+    from = edges$Source,
+    to = edges$Target,
+    weight = edges$Weight)
+  net4<-as.network(a1)
+  speaker<-paste0("klemm_",nodes$id)
+  nodes$speaker<-speaker  
+nodes
