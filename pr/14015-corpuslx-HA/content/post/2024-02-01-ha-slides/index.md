72c6a03f171607ff4447796bc4da098c22adc264
estee <91368979+esteeschwarz@users.noreply.github.com>
2024-02-01 21:02:59 +0100

    site & gitbook essais, hu-lu wks

---
+++ pr/14015-corpuslx-HA/content/post/2024-02-01-ha-slides/index.md
@@ -0,0 +1,8 @@
+---
+title: HA-slides
+author: R package build
+date: '2024-02-01'
+slug: ha-slides
+categories: []
+tags: []
+---
---
+++ pub/sites/rendermdbook.R
@@ -0,0 +1,27 @@
+mdf<-list.files("~/Documents/GitHub/HU-LX/mdbook/workflow/src")
+mdf.cl<-gsub("\\..*","",mdf)
+mdf.rmd<-paste0(mdf.cl,".Rmd")
+#mdf
+#mdtemp<-tempdir("tmd")
+k<-1
+#sort(mdf)
+m<-grepl("SUMMARY",mdf)
+mdf<-mdf[!m]
+mdf.d<-paste0("~/Documents/GitHub/HU-LX/mdbook/workflow/src/",mdf)
+for(k in 1:length(mdf)){
+  md<-readLines(mdf.d[k])
+  m<-grep("([0-9]{1,2}\\])",md)
+  rep<-paste0(k,"-\\1")
+  fig<-gsub("([0-9]{1,2}\\])",rep,md[m])
+  md[m]<-fig
+  mdns<-paste0("mod/",mdf[k])
+  mdns<-paste0("mod/",mdf.cl[k],".Rmd")
+  mdns<-mdf.rmd[k]
+  writeLines(md,mdns)
+}
+render("index.Rmd")
+bookdown::bookdown_site("index.Rmd")
+library(blogdown)
+hulxfiles<-list.files("~/Documents/GitHub/DH_essais/pub/hu-lx/site2/src")
+hulxrmds<-paste0("~/Documents/GitHub/DH_essais/pub/hu-lx/site2/src",hulxfiles)
+
